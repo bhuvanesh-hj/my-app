47b4abf532036742b66db3e21756217e86ae9727
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-09-28 18:11:03 +0530

    added event on button

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -5,12 +5,16 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate.js";
 import ExpenseDetails from "./ExpenseDetails.js";
 const ExpenseItem = (props) => {
+  const deleteExpense=()=>{
+      console.log("Delete Expense")
+  }
   return (
-    <Card className="expense-item">
+    <Card className="expense-item" id={props.title}>
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
         <ExpenseDetails title={props.title} location={props.location} amount={props.amount} />
         </div>
+        <button onClick={deleteExpense}>Delete</button>
     </Card>
   );
 }
