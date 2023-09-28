ebb32851cc4abd1d47365c13918fed196518421d
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-09-28 20:17:23 +0530

    updated the price of the expense by useState

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,17 +4,26 @@ import "./ExpenseDetails.js";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate.js";
 import ExpenseDetails from "./ExpenseDetails.js";
+import { useState } from "react";
 const ExpenseItem = (props) => {
-  const deleteExpense=()=>{
-      console.log("Delete Expense")
-  }
+ 
+    //this return the array
+    // this array cotaines in the first the state inital vlaue and updateing function
+      const [amount,setAmount] = useState(props.amount);
+
+      const UpdateExpenseAmount = ()=>{
+        setAmount(100);
+        // console.log(amount);
+      }
+
   return (
     <Card className="expense-item" id={props.title}>
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-        <ExpenseDetails title={props.title} location={props.location} amount={props.amount} />
+          {/* for paricular state we have initialize with the state returning initial value to the element */}
+        <ExpenseDetails title={props.title} location={props.location} amount={amount} />
         </div>
-        <button onClick={deleteExpense}>Delete</button>
+        <button onClick={UpdateExpenseAmount}>Update Expense amount</button>
     </Card>
   );
 }
