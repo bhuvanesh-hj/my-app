3a86061fc3ea37f5d66e23344a8c9185f0ed5e40
bhuvanesh-hj <144657401+bhuvanesh-hj@users.noreply.github.com>
2023-10-01 22:18:08 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+import AddUser from "./Components/user/AddUser.jsx";
+import UserList from "./Components/user/UserList";
+function App() {
+  const [userList, setUserList] = useState([]);
+  const getUser = (name, age) => {
+    setUserList((prev) => {
+      return [
+        ...prev,
+        {
+          Username: name,
+          Age: age,
+          id: userList.length === 0 ? 1 : userList[userList.length - 1].id + 1,
+        },
+      ];
+    });
+  };
+  const deleteUser = (uid) => {
+      setUserList(userList.filter((user) => user.id !== uid));
+    };
+  return (
+    <>
+      <AddUser getuser={getUser} />
+      <UserList user={userList} delete={deleteUser} />
+    </>
+  );
+}
+
+export default App;
